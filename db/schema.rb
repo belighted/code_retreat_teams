60fe14a4a5b6cc3c6f2e182f66988c94bb4bbb4b
Sergei Krylov <sergei.krylov@yahoo.com>
2022-07-20 14:00:10 +0300

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,36 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_20_101418) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_20_105512) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "code_retreat_sessions", force: :cascade do |t|
+    t.date "performed_on"
+    t.string "title"
+    t.string "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "participations", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "code_retreat_session_id", null: false
+    t.bigint "primary_technology_id", null: false
+    t.bigint "secondary_technology_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["code_retreat_session_id"], name: "index_participations_on_code_retreat_session_id"
+    t.index ["primary_technology_id"], name: "index_participations_on_primary_technology_id"
+    t.index ["secondary_technology_id"], name: "index_participations_on_secondary_technology_id"
+    t.index ["user_id"], name: "index_participations_on_user_id"
+  end
+
+  create_table "technologies", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -21,4 +48,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_20_101418) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "participations", "code_retreat_sessions"
+  add_foreign_key "participations", "technologies", column: "primary_technology_id"
+  add_foreign_key "participations", "technologies", column: "secondary_technology_id"
+  add_foreign_key "participations", "users"
 end
