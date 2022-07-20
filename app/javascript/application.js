211617399c8d02f9b62b3a8539afa6d1d1b9d90f
Sergei Krylov <sergei.krylov@yahoo.com>
2022-07-20 12:42:36 +0300

    Setup react

---
+++ app/javascript/application.js
@@ -0,0 +1,6 @@
+// Entry point for the build script in your package.json
+import "@hotwired/turbo-rails"
+import "./controllers"
+
+// Entry point for the build script in your package.json
+import "./react/src/index.js"
