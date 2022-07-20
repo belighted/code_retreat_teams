3a883759f146a6be24ce368ee4c205c11fc3cd20
Sergei Krylov <sergei.krylov@yahoo.com>
2022-07-20 14:48:45 +0300

    Make root path render the app component

---
+++ app/javascript/react/src/components/App.jsx
@@ -0,0 +1,13 @@
+import * as React from 'react'
+import * as ReactDOM from 'react-dom'
+
+const App = () => {
+  return (<div>THIS IS THE APP!!!!</div>)
+}
+
+// Use it if you don't plan to use "remount"
+// document.addEventListener('DOMContentLoaded', () => {
+// ReactDOM.render(<Hello />, document.getElementById('hello'))
+// })
+
+export default App
--- app/javascript/react/src/index.js
+++ app/javascript/react/src/index.js
@@ -1,4 +1,6 @@
 import { define } from 'remount'
-import Hello from "./components/Hello"
+// import Hello from "./components/Hello"
+import App from "./components/App"
 
-define({ 'hello-component': Hello })
+// define({ 'hello-component': Hello })
+define({ 'app-component': App })
